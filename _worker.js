08e0c15817a032abd65c1ef6c658f8962f6ae6ea
vbarter <yzcaijunjie@gmail.com>
2023-05-05 17:27:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "api.openai.com";
-    // openai is already set all CORS heasders 
+    url.host = "www.discord.com";
     return fetch(url, {
       headers: request.headers,
       method: request.method,
