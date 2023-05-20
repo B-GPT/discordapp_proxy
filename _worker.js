db3b2a4aff12d3aef315da33a6cfa4e251e1fcea
vbarter <yzcaijunjie@gmail.com>
2023-05-20 11:31:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "www.discord.com";
+    url.host = "cdn.discordapp.com";
     return fetch(url, {
       headers: request.headers,
       method: request.method,
